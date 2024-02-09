34ed331edabde16e8e240923c93fde884dab9d9f
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-02-09 07:09:04 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ilk sayı: ");
+        a = scanner.nextInt();
+
+        System.out.println("ikinci sayı: ");
+        b = scanner.nextInt();
+
+        System.out.println("ucuncu sayı: ");
+        c = scanner.nextInt();
+
+        if (a > b && a > c) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            }
+            if (c > b) {
+                System.out.println("a > c > b");
+            }
+        } else if (b > a && b > c) {
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else {
+                System.out.println("b > c > a");
+            }
+        } else{
+            if(a>b){
+                System.out.println("c > a > b");
+            }else{
+                System.out.println("c > b > a");
+            }
+        }
+
+    }
+}
